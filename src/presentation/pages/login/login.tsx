e8e1c73456b8896f60d9c877e053f05aa5c077e0
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-05 20:12:52 -0300

    feat: add login view

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,34 @@
 import React from 'react'
 
+import Styles from './styles.scss'
+import { Spinner } from '@/presentation/components'
+
 const Login: React.FC = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className={Styles.login}>
+      <header className={Styles.header}>
+        <h1>Language App</h1>
+      </header>
+      <form className={Styles.form}>
+        <h2>Login</h2>
+        <div className={Styles.inputWrap}>
+          <input type="email" name="email" placeholder="Email" />
+          <span className={Styles.status}>* Error</span>
+        </div>
+        <div className={Styles.inputWrap}>
+          <input type="password" name="password" placeholder="Password" />
+          <span className={Styles.status}>* Error</span>
+        </div>
+        <button className={Styles.submit} type="submit">
+          Sign in
+        </button>
+        <span className={Styles.link}>Create account</span>
+        <div className={Styles.errorWrap}>
+          <Spinner className={Styles.spinner} />
+          <span className={Styles.error}>Error</span>
+        </div>
+      </form>
+      <footer className={Styles.footer} />
     </div>
   )
 }
