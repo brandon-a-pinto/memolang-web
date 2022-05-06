82a55d58bb6bc224abd8507418722413c341e7d1
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-05 21:10:45 -0300

    refactor: move input to components

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Spinner } from './spinner/spinner'
 export { default as Header } from './header/header'
 export { default as Footer } from './footer/footer'
+export { default as Input } from './input/input'
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Styles from './styles.scss'
-import { Spinner, Header, Footer } from '@/presentation/components'
+import { Spinner, Header, Footer, Input } from '@/presentation/components'
 
 const Login: React.FC = () => {
   return (
@@ -9,14 +9,8 @@ const Login: React.FC = () => {
       <Header />
       <form className={Styles.form}>
         <h2>Login</h2>
-        <div className={Styles.inputWrap}>
-          <input type="email" name="email" placeholder="Email" />
-          <span className={Styles.status}>* Error</span>
-        </div>
-        <div className={Styles.inputWrap}>
-          <input type="password" name="password" placeholder="Password" />
-          <span className={Styles.status}>* Error</span>
-        </div>
+        <Input type="email" name="email" placeholder="Email" />
+        <Input type="password" name="password" placeholder="Password" />
         <button className={Styles.submit} type="submit">
           Sign in
         </button>
