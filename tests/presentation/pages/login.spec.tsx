11c3681bc24862d24f209077859fc417e41fdb9a
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-08 12:26:04 -0300

    test: ensure Login enables submit button if form is valid

--- tests/presentation/pages/login.spec.tsx
+++ tests/presentation/pages/login.spec.tsx
@@ -70,4 +70,17 @@ describe('Login Component', () => {
     const passwordStatus = sut.getByTestId('password-status')
     expect(passwordStatus.textContent).toBe('')
   })
+
+  it('should enable submit button if form is valid', async () => {
+    const { sut, validationStub } = makeSut()
+    validationStub.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, {
+      target: { value: faker.internet.password() }
+    })
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(false)
+  })
 })
