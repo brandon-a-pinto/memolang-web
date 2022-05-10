44a1c8de574432835fef771431711784f447ee6d
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-10 20:43:56 -0300

    test: ensure ValidationBuilder returns EmailValidation

--- tests/validation/validators/validation-builder.spec.ts
+++ tests/validation/validators/validation-builder.spec.ts
@@ -2,7 +2,8 @@ import faker from '@faker-js/faker'
 
 import {
   ValidationBuilder as sut,
-  RequiredFieldValidation
+  RequiredFieldValidation,
+  EmailValidation
 } from '@/validation/validators'
 
 const field = faker.random.words()
@@ -11,5 +12,10 @@ describe('ValidationBuilder', () => {
   it('should return RequiredFieldValidation', () => {
     const validations = sut.field(field).required().build()
     expect(validations).toEqual([new RequiredFieldValidation(field)])
+  })
+
+  it('should return EmailValidation', () => {
+    const validations = sut.field(field).email().build()
+    expect(validations).toEqual([new EmailValidation(field)])
   })
 })
