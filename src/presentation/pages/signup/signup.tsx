a9553b14f46e6fba7555ec8e272fa618c5e400ca
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-13 15:51:41 -0300

    feat: ensure SignUp starts with correct initial state

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,15 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 
 import Styles from './styles.scss'
 import { FormStatus, Header, Footer, Input } from '@/presentation/components'
 import { FormContext } from '@/presentation/contexts'
 
+type StateProps = {
+  emailError: string
+  usernameError: string
+  passwordError: string
+  passwordConfirmationError: string
+}
+
 const SignUp: React.FC = () => {
+  const [state, setState] = useState<StateProps>({
+    emailError: 'Invalid field',
+    usernameError: 'Invalid field',
+    passwordError: 'Invalid field',
+    passwordConfirmationError: 'Invalid field'
+  })
+
   return (
     <div className={Styles.signup}>
       <Header />
-      <FormContext.Provider value={{ state: {} }}>
+      <FormContext.Provider value={{ state, setState }}>
         <form data-testid="form" className={Styles.form}>
           <h2>Sign up</h2>
           <Input type="email" name="email" placeholder="Email" />
@@ -20,7 +34,12 @@ const SignUp: React.FC = () => {
             name="passwordConfirmation"
             placeholder="Confirm Password"
           />
-          <button type="submit" className={Styles.submit}>
+          <button
+            data-testid="submit"
+            type="submit"
+            disabled
+            className={Styles.submit}
+          >
             Create account
           </button>
           <Link to="/login" className={Styles.link}>
