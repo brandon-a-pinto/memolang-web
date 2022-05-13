c408d8a7ac1b1cf04b504b4a62de0202dd67b261
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-13 19:53:15 -0300

    test: ensure SignUp shows email error if Validation fails

--- tests/presentation/pages/signup.spec.ts
+++ tests/presentation/pages/signup.spec.ts
@@ -1,26 +1,42 @@
-import { screen } from '@testing-library/react'
+import faker from '@faker-js/faker'
+import { screen, cleanup } from '@testing-library/react'
 import { createMemoryHistory } from 'history'
 
 import { SignUp } from '@/presentation/pages'
-import { renderWithHistory, Helper } from '@/tests/presentation/mocks'
+import {
+  renderWithHistory,
+  Helper,
+  ValidationStub
+} from '@/tests/presentation/mocks'
 
 const history = createMemoryHistory({ initialEntries: ['/signup'] })
-const makeSut = (): void => {
+const makeSut = (validationError?: string): void => {
+  const validationStub = new ValidationStub()
+  validationStub.errorMessage = validationError
   renderWithHistory({
     history,
-    Page: () => SignUp({})
+    Page: () => SignUp({ validation: validationStub })
   })
 }
 
 describe('SignUp Component', () => {
+  afterEach(cleanup)
+
   it('should start with initial state', () => {
-    const validationError = 'Invalid field'
-    makeSut()
+    const validationError = faker.random.words()
+    makeSut(validationError)
     expect(screen.getByTestId('error-wrap').children).toHaveLength(0)
     expect(screen.getByTestId('submit')).toBeDisabled()
     Helper.testStatusForField('email', validationError)
     Helper.testStatusForField('username', validationError)
     Helper.testStatusForField('password', validationError)
+    Helper.testStatusForField('passwordConfirmation', validationError)
+  })
+
+  it('should show email error if Validation fails', () => {
+    const validationError = faker.random.words()
+    makeSut(validationError)
+    Helper.populateField('email')
     Helper.testStatusForField('email', validationError)
   })
 })
