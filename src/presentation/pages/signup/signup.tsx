1d600d055b8cadc26480d65cbe1a9c386543d7e2
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-14 17:39:52 -0300

    feat: ensure SignUp shows spinner on submit

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 import Styles from './styles.scss'
 import { FormStatus, Header, Footer, Input } from '@/presentation/components'
 import { FormContext } from '@/presentation/contexts'
 import { Validation } from '@/presentation/contracts'
+import { AddAccount } from '@/domain/usecases'
 
 type Props = {
   validation: Validation
+  addAccount: AddAccount
 }
 
 type StateProps = {
   isLoading: boolean
+  errorMessage: string
   email: string
   emailError: string
   username: string
@@ -22,9 +25,11 @@ type StateProps = {
   passwordConfirmationError: string
 }
 
-const SignUp: React.FC<Props> = ({ validation }: Props) => {
+const SignUp: React.FC<Props> = ({ validation, addAccount }: Props) => {
+  const navigate = useNavigate()
   const [state, setState] = useState<StateProps>({
     isLoading: false,
+    errorMessage: '',
     email: '',
     emailError: '',
     username: '',
@@ -48,11 +53,42 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
     })
   }, [state.email, state.username, state.password, state.passwordConfirmation])
 
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    try {
+      event.preventDefault()
+      if (state.isLoading || state.emailError || state.passwordError) {
+        return
+      }
+      setState({
+        ...state,
+        isLoading: true
+      })
+      await addAccount.add({
+        email: state.email,
+        username: state.username,
+        password: state.password,
+        passwordConfirmation: state.passwordConfirmation
+      })
+      navigate('/login')
+    } catch (err) {
+      setState({
+        ...state,
+        isLoading: false,
+        errorMessage: err.message
+      })
+    }
+  }
   return (
     <div className={Styles.signup}>
       <Header />
       <FormContext.Provider value={{ state, setState }}>
-        <form data-testid="form" className={Styles.form}>
+        <form
+          data-testid="form"
+          className={Styles.form}
+          onSubmit={handleSubmit}
+        >
           <h2>Sign up</h2>
           <Input type="email" name="email" placeholder="Email" />
           <Input type="text" name="username" placeholder="Username" />
