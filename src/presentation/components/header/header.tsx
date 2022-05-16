ccabd68d045c6402ee17537b4f62877fa6740950
Brandon Pinto <brandon.a.pinto@protonmail.com>
2022-05-16 19:10:31 -0300

    refactor: add links to header

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,11 +1,22 @@
 import React, { memo } from 'react'
+import { Link } from 'react-router-dom'
 
 import Styles from './styles.scss'
 
 const Header: React.FC = () => {
   return (
     <header className={Styles.header}>
-      <h1>Language App</h1>
+      <Link to="/" className={Styles.link}>
+        Memolang
+      </Link>
+      <div>
+        <Link to="/login" className={Styles.link}>
+          Login
+        </Link>
+        <Link to="/signup" className={Styles.link}>
+          Register
+        </Link>
+      </div>
     </header>
   )
 }
